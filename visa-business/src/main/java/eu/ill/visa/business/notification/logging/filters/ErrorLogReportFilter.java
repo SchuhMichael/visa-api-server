e49244a27e1b4ed554ea264a63daef2de7c3be3b
Michael Schuh <michael.schuh@xfel.eu>
2024-07-25 08:54:45 +0200

    testing use of oidc.token-state-manager

--- visa-business/src/main/java/eu/ill/visa/business/notification/logging/filters/ErrorLogReportFilter.java
+++ visa-business/src/main/java/eu/ill/visa/business/notification/logging/filters/ErrorLogReportFilter.java
@@ -15,7 +15,7 @@ import java.util.logging.Filter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
-@LoggingFilter(name = "error-email-filter")
+//   @LoggingFilter(name = "error-email-filter")
 public class ErrorLogReportFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(ErrorLogReportFilter.class);
